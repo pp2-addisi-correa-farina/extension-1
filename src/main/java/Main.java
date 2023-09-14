7ffb0201c3c5cba288ba6d436e9f17b46839b80c
lucas <lucas.addisi@gmail.com>
2023-09-14 00:12:42 -0300

    Se actualiza a la nueva versión del core

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,20 @@
 import org.pp2.Dispositivo;
 import org.pp2.DriverClimatizador;
+import org.pp2.comando.Interprete;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
 
 public class Main {
     public static void main(String[] args) {
-        Dispositivo dispositivo = new Dispositivo("id1", "Aire Cocina", "Samsung v3");
-        if (!new SamsungDriverClimatizadorFactory().isCompatible(dispositivo)) throw new RuntimeException("Dispositivo No Compatible");
-        DriverClimatizador driverClimatizador = new SamsungDriverClimatizadorFactory().create();
-        driverClimatizador.establecerTemperatura(dispositivo, 21);
+        // FIXME: Esto está mal
+        DriverClimatizador driverClimatizador = new SamsungDriverClimatizador();
+        Map<String, BiConsumer<DriverClimatizador, Dispositivo>> nombreComandoEjecutorMap = new HashMap<>();
+        nombreComandoEjecutorMap.put("ENCENDER", DriverClimatizador::encender);
+        nombreComandoEjecutorMap.put("ENFRIAR", DriverClimatizador::enfriar);
+
+        Dispositivo dispositivo = new Dispositivo("id1", "Aire Cocina", "Samsung v3", new Interprete(driverClimatizador, nombreComandoEjecutorMap));
+        dispositivo.ejecutar("ENCENDER");
     }
 }
--- src/main/java/SamsungDriverClimatizador.java
+++ src/main/java/SamsungDriverClimatizador.java
@@ -3,8 +3,14 @@ import org.pp2.DriverClimatizador;
 
 public class SamsungDriverClimatizador implements DriverClimatizador {
     @Override
-    public void establecerTemperatura(Dispositivo dispositivo, int temperatura) {
-        System.out.printf("Integración SAMSUNG: Estableciendo temperatura del dispositivo llamado \"%s\" modelo \"%s\" a %s grados%n",
-                dispositivo.getNombre(), dispositivo.getModelo(), temperatura);
+    public void enfriar(Dispositivo dispositivo) {
+        System.out.printf("Integración SAMSUNG: Configurando dispositivo llamado \"%s\" modelo \"%s\" en modo enfriar.",
+                dispositivo.getNombre(), dispositivo.getModelo());
+    }
+
+    @Override
+    public void encender(Dispositivo dispositivo) {
+        System.out.printf("Integración SAMSUNG: Encendiendo dispositivo llamado \"%s\" modelo \"%s\" ",
+                dispositivo.getNombre(), dispositivo.getModelo());
     }
 }
